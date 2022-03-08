470d98a12f52259f04e5a326123a90c16cf32788
Thyago Nunes <devthyagonunes@gmail.com>
2022-03-07 23:08:48 -0300

    routes, server and middleware route

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -2,10 +2,12 @@ import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 app.use(routes);
 
 app.use(
