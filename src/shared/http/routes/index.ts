3fd23385f0adc06ab811058b2a55fbbb8f52bd60
THYAGO OLIVEIRA NUNES <thyago.nunes@hss.local>
2022-03-08 16:12:22 -0300

    change message route '/'

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const routes = Router();
 
 routes.get('/', (request, response) => {
-  response.json({ message: 'Boa tarde seu Lindo você é pica 💘' });
+  response.json({ message: 'Boa tarde seu Lindo, você é pica 💘' });
 });
 
 export default routes;
