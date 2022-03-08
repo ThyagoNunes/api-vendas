80d10e18d6a57dc66fc7d0219076eccc24d00e7e
THYAGO OLIVEIRA NUNES <thyago.nunes@hss.local>
2022-03-08 19:08:34 -0300

    yarn add typeorm, reflect-metadata & pg

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,6 @@
+import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
-
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
 
