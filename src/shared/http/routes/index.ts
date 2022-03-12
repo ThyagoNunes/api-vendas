d89b53fd51da5348bfe845861ff43a7207996ecf
Thyago Nunes <devthyagonunes@gmail.com>
2022-03-12 14:54:14 -0300

    #EOSGURI

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 
 const routes = Router();
 routes.get('/', (request, response) => {
-  response.json({ message: 'Você está em processo de aprendizagem 💘 ' });
+  response.json({ message: '🔥 É US GURI 🔥 ' });
 });
 
 export default routes;
