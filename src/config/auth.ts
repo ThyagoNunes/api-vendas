311ea2905b62c2987622fc4d917b14bc059ae958
Thyago Nunes <devthyagonunes@gmail.com>
2022-03-25 14:45:11 -0300

    -C authorization JWT and auth.ts :fire:

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'e71d429244868a39e3af20c464c1aef4',
+    expiresIn: '1d',
+  },
+};
--- src/modules/users/services/CreateSessionsService.ts
+++ src/modules/users/services/CreateSessionsService.ts
@@ -1,23 +1,25 @@
 import AppError from '@shared/errors/AppError';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+import authConfig from '@config/auth';
 import { getCustomRepository } from 'typeorm';
 import User from '@modules/users/typeorm/entities/User';
 import UsersRepository from '@modules/users/typeorm/repositories/UsersRepository';
-import { compare } from 'bcryptjs';
 
 interface IRequest {
   email: string;
   password: string;
 }
-/*
 interface IResponse {
   user: User;
-} */
+  token: string;
+}
 
 /*serviços deverão ter uma única funcionalidade
 e com isso único método e ele irá executar essa responsabilidade específica do serviço */
 
 class CreateSessionsService {
-  public async execute({ email, password }: IRequest): Promise<User> {
+  public async execute({ email, password }: IRequest): Promise<IResponse> {
     const usersRepository = getCustomRepository(UsersRepository);
     const user = await usersRepository.findByEmail(email);
 
@@ -31,8 +33,20 @@ class CreateSessionsService {
     if (!passwordConfirmed) {
       throw new AppError('Incorrect email/password combination.', 401);
     }
+    /*
+    1ºparam = payload ( nada de dados sensíveis )
+    2ºparam = hash => secret = 'e71d429244868a39e3af20c464c1aef4' -> md5.cz
+    3ºparam = object with more configs for token
+*/
+    const token = sign({}, authConfig.jwt.secret, {
+      subject: user.id,
+      expiresIn: authConfig.jwt.expiresIn,
+    });
 
-    return user;
+    return {
+      user,
+      token,
+    };
   }
 }
 
